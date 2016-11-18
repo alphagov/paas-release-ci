28f6e0cf60b3356d93c663280c3f7efa74c23148
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-11-18 16:14:34 +0000

    First steps for setup pipeline.
    This will setup the necessary dependencies for running other pipelines
    for creating boshreleases. At present it merely contains a job to
    self-update.
    
    This also includes all the necessary scripts to login to concourse, and
    upload a pipeline.

---
+++ scripts/deploy-pipeline.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -eu
+
+SCRIPT=$0
+
+usage() {
+   cat <<EOF
+Usage:
+
+   $SCRIPT <pipeline> <config> <varsfile>
+
+Being:
+
+   pipeline   pipeline name
+   config	    config for the pipeline
+   varsfile   concourse variables to pass to the pipeline
+
+EOF
+   exit 1
+}
+
+if [ $# -lt 3 ]; then
+   usage
+fi
+
+pipeline=$1; shift
+config=$1; shift
+varsfile=$1; shift
+
+echo "Concourse API target: ${FLY_TARGET}"
+echo "Pipeline: ${pipeline}"
+echo "Config file: ${config}"
+
+yes y | \
+   $FLY_CMD -t "${FLY_TARGET}" \
+   set-pipeline \
+   --config "${config}" \
+   --pipeline "${pipeline}" \
+   --load-vars-from "${varsfile}"
+
+$FLY_CMD -t "${FLY_TARGET}" \
+  unpause-pipeline \
+  --pipeline "${pipeline}"
+
+if [ -n "${EXPOSE_PIPELINE:-}" ]; then
+  $FLY_CMD -t "${FLY_TARGET}" \
+    expose-pipeline \
+    --pipeline "${pipeline}"
+fi
