32b6961cf2c478625b81884e928a47989f7fee17
Jonathan Matthews <jonathan.matthews@digital.cabinet-office.gov.uk>
2018-09-21 10:54:01 +0100

    Make fly-sync wrapper able to upgrade v3 -> v4
    A Concourse `fly` binary v3.x cannot, in every situation, `fly sync`
    itself to a v4.x version (see [1]). This commit:
    
    - changes our wrapper script's logic so that it *can* sync itself across
      these major versions and
    - puts the logic into consumable functions, for clarity.
    
    This removes our previous file timestamp-based logic, which isn't
    needed.
    
    [1] https://concourse-ci.org/download.html#v400

--- scripts/fly_sync_and_login.sh
+++ scripts/fly_sync_and_login.sh
@@ -8,16 +8,40 @@ set -euo pipefail
 # FLY_CMD
 # FLY_TARGET
 
-FLY_CMD_URL="${CONCOURSE_URL}/api/v1/cli?arch=amd64&platform=$(uname | tr '[:upper:]' '[:lower:]')"
-echo "Downloading fly command..."
-if [ -f "$FLY_CMD" ]; then
-  timestamp_flag=1
+fetch_fly() {
+  echo "Downloading fly .."
+  FLY_CMD_URL="${CONCOURSE_URL}/api/v1/cli?arch=amd64&platform=$(uname | tr '[:upper:]' '[:lower:]')"
+  curl \
+    --progress-bar \
+    --location \
+    --fail \
+    --output "$FLY_CMD" \
+    "$FLY_CMD_URL"
+  chmod +x "$FLY_CMD"
+}
+
+fly_sync() {
+  echo "Doing fly sync .."
+  $FLY_CMD -t "${FLY_TARGET}" sync
+}
+
+fly_login() {
+  echo "Doing fly login .."
+  $FLY_CMD -t "${FLY_TARGET}" login --concourse-url "${CONCOURSE_URL}" -u "${CONCOURSE_ATC_USER}" -p "${CONCOURSE_ATC_PASSWORD}"
+}
+
+fly_is_runnable() {
+  [ -x "${FLY_CMD}" ]
+}
+
+if fly_is_runnable; then
+  if fly_login; then
+    fly_sync
+  else
+    fetch_fly
+    fly_login
+  fi
+else
+  fetch_fly
+  fly_login
 fi
-curl "$FLY_CMD_URL" -# -L -f ${timestamp_flag:+-z "$FLY_CMD"} -o "$FLY_CMD" -u "${CONCOURSE_ATC_USER}:${CONCOURSE_ATC_PASSWORD}"
-chmod +x "$FLY_CMD"
-
-echo "Doing fly login"
-$FLY_CMD -t "${FLY_TARGET}" login --concourse-url "${CONCOURSE_URL}" -u "${CONCOURSE_ATC_USER}" -p "${CONCOURSE_ATC_PASSWORD}"
-
-echo "Doing fly sync"
-$FLY_CMD -t "${FLY_TARGET}" sync
