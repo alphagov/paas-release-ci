432e1c36dd3098c12ba188ecf23e012a560a9697
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2020-10-29 12:24:28 +0000

    Fix terraform to run with 0.13
    There were a couple of issues with interpolation of variables and the
    plugin syntax needed updating.

--- terraform/buckets-boshreleases.tf
+++ terraform/buckets-boshreleases.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "gds-paas-releases" {
-  bucket        = "${var.releases_bucket_name}"
+  bucket        = var.releases_bucket_name
   acl           = "public-read"
   force_destroy = "true"
 }
 
 resource "aws_s3_bucket" "gds-paas-releases-blobs" {
-  bucket = "${var.releases_blobs_bucket_name}"
+  bucket = var.releases_blobs_bucket_name
   acl    = "public-read"
 
   policy = <<POLICY
---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1.2"
+    }
+
+  }
+  required_version = ">= 0.13"
+}
